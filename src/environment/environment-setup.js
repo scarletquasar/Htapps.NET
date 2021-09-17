fd4075e9a3a151a0793ce13ec5715a0bd021d1d2
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-17 17:15:42 -0300

    DevUpdate

---
+++ src/environment/environment-setup.js
@@ -0,0 +1,2 @@
+document.head = document.getElementById("_app-head");
+document.body.style.fontFamily = "Roboto";
---
+++ start.bat
@@ -0,0 +1,2 @@
+"C:\Users\User\Desktop\Outros\htapps\bin\mshta.exe" "C:\Users\User\Desktop\Outros\htapps\test.hta"
+pause
