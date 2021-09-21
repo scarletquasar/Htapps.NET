1d7593b9e0f60a1c7e08a0ca1a1854433c41cded
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-21 15:17:18 -0300

    Console Errors and Disable Context Menu

--- environment/modules/libraries/environment-setup.js
+++ environment/modules/libraries/environment-setup.js
@@ -13,5 +13,15 @@ console = {
     log: function() {
         var args = Array.from(arguments).toString();
         console.output.push(args);
-    }    
+    },
+
+    error: function() {
+        var args = Array.from(arguments).toString();
+        console.output.push("[X] Error: " + args);
+    },
+};
+
+window.onerror = function(e) {
+    console.error(e);
+    e.preventDefault();
 };
