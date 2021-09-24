27a5c88a325fc5c4652764be2e60ec3b7d65afbf
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-24 13:28:22 -0300

    htapps cli base

---
+++ src/htapps-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace htapps_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Htapps CLI Base - In Development]");
+        }
+    }
+}
