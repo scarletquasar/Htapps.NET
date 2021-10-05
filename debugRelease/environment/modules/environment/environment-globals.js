a118ab3bba168d8db80d3484c6d16f2eb5ee8ffd
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-04 21:57:15 -0300

    Zipped debugRelease

--- debugRelease/environment/modules/environment/environment-globals.js
+++
@@ -1,5 +0,0 @@
-/*
-    bindGlobals - bindGlobals is a way of storing temporary data that needs a unique naming, 
-    it is usually used by built-in asynchronous operators.
-*/
-var bindGlobals = {};
